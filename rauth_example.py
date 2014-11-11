ad00c6c83259343bb9ab4ecba9dd71a93f794514
asmaier <asmaier@web.de>
2014-11-11 17:31:35 +0100

    Added parameter to example request

--- rauth_example.py
+++ rauth_example.py
@@ -7,7 +7,7 @@ from rauth import OAuth1Session
 session = OAuth1Session(consumer_key='hackathon', consumer_secret='33f3fb11-59b4-4a38-a549-27bb4628d1af')
 # it's important to enable header based authentification, URL parameter signature will not work
 # to ignore SSL certificate verify failed error by setting verify parameter to False
-r = session.get('https://ipool.s.asideas.de/api/v3/search', header_auth=True, verify=False)
+r = session.get('https://ipool.s.asideas.de/api/v3/search', params={'q':'"Merkel"'}, header_auth=True, verify=False)
 # r is an Response object; if you get <Response [200]> you're good
 # have a look at https://github.com/kennethreitz/requests
 print r
